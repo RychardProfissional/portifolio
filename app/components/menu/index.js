586b0dd7415b51a399a2e03343ad4b2eb2a85dbe
Só mais um programador <123836822+RychardProficional@users.noreply.github.com>
2023-08-02 08:35:45 +0000

    restart

---
+++ app/components/menu/index.js
@@ -0,0 +1,16 @@
+export default function Menu()
+{
+    return (
+        <div className="container-menu">
+            <main className="info">
+                <h1>Rychard Antony Pereira de Arruda</h1>
+            </main>
+            <nav className="menu">
+                <li className="item"><a className="link">seila</a></li>
+                <li className="item"><a className="link">seilasfasdfa</a></li>
+                <li className="item"><a className="link">seilaasdf</a></li>
+                <li className="item"><a className="link">seilaa</a></li>
+            </nav>
+        </div>
+    )
+}
